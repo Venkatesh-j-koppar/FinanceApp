ee6eda52595f3e96701e22b9f93d383bbab21413
Venkatesh <venkateshkoppar@gmail.com>
2022-05-31 16:42:52 +0530

    app-created

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const App=()=>{
+  return(
+    <div className="container">
+    <h1 className="mt-3">My Budget Planner</h1>   
+    <div className="row mt-3">
+      <div className="col-sm">
+        
+      </div>
+      </div>   
+
+    </div>
+  )
+}
+
+export default App;
